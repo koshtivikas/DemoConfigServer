52fa4930900d9923b7081482c34173ed32e760e5
koshtivikas <koshti.vikas@gmail.com>
2019-01-31 20:36:14 -0600

    Demo config Server application

---
+++ src/test/java/com/java/configserver/DemoConfigServer/DemoConfigServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.java.configserver.DemoConfigServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
